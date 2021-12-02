0a93e87780f668d2dd43b7a990391d9328c79b75
JoTerrance <gorka.zgz@gmail.com>
2021-12-02 01:53:30 +0100

    disabled core

--- Spacevim/init.lua
+++ Spacevim/init.lua
@@ -1,5 +1,71 @@
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
+-- following options are the default
+-- each of these are documented in `:help nvim-tree.OPTION_NAME`
+local tree_cb = require'nvim-tree.config'.nvim_tree_callback
+require'nvim-tree'.setup {
+  disable_netrw       = true,
+  hijack_netrw        = true,
+  open_on_setup       = true,
+  ignore_ft_on_setup  = {},
+  auto_close          = false,
+  open_on_tab         = false,
+  hijack_cursor       = false,
+  update_cwd          = false,
+  update_to_buf_dir   = {
+    enable = true,
+    auto_open = true,
+  },
+  diagnostics = {
+    enable = false,
+    icons = {
+      hint = "",
+      info = "",
+      warning = "",
+      error = "",
+    }
+  },
+  update_focused_file = {
+    enable      = false,
+    update_cwd  = false,
+    ignore_list = {}
+  },
+  system_open = {
+    cmd  = nil,
+    args = {}
+  },
+  filters = {
+    dotfiles = false,
+    custom = {}
+  },
+  git = {
+    enable = true,
+    ignore = true,
+    timeout = 500,
+  },
+  view = {
+    width = 30,
+    height = 30,
+    hide_root_folder = false,
+    side = 'reght',
+    auto_resize = false,
+    mappings = {
+      custom_only = false,
+      list = {
+         { 
+         key = {"<C-g>"},    
+       cb = tree_cb("cd"), mode = "n"
+   }
+      }
+    },
+    number = false,
+    relativenumber = false
+  },
+  trash = {
+    cmd = "trash",
+    require_confirm = true
+  }
+}
 local wk = require('whichkey_setup')
 
 local normal_keymap={
