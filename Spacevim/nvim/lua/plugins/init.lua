6a158e48c724e8b54648ac62b2b97ce7774961fb
JoTerrance <gorka.zgz@gmail.com>
2022-04-15 14:50:15 +0200

    updated nvim

--- Spacevim/nvim/lua/plugins/init.lua
+++ Spacevim/nvim/lua/plugins/init.lua
@@ -3,6 +3,12 @@ return require('packer').startup(function(use)
 -- Packer can manage itself
   use 'wbthomason/packer.nvim'
   use {'nvim-treesitter/nvim-treesitter', run = ":TSUpdate", event = "BufWinEnter", config = "require('treesitter-config')"}
+  use {
+    'tamton-aquib/staline.nvim',
+    requires = {'kyazdani42/nvim-web-devicons', opt = true},
+    event = "BufRead",
+    config = "require('staline-config')"
+  }
   use {'akinsho/bufferline.nvim', requires = 'kyazdani42/nvim-web-devicons'}
   use {'kyazdani42/nvim-tree.lua', requires = 'kyazdani42/nvim-web-devicons' }
   use {'windwp/nvim-ts-autotag', event = "InsertEnter", after = "nvim-treesitter"}
@@ -20,14 +26,22 @@ return require('packer').startup(function(use)
   use {'norcalli/nvim-colorizer.lua', config = "require('colorizer-config')", event = "BufRead"}
   use {
     'lewis6991/gitsigns.nvim',
-    requires = {'nvim-lua/plenary.nvim'}
+    requires = {'nvim-lua/plenary.nvim'},
+    config = function()
+      require('gitsigns').setup {current_line_blame = true}
+    end
   }
   use {'glepnir/dashboard-nvim', cmd = "Dashboard", config = "require('dashboard-config')"}
   use {"lukas-reineke/indent-blankline.nvim", config = "require('blankline-config')", event = "BufRead"}
   use {'lukas-reineke/format.nvim', config = "require('format-config')", cmd = "Format"}
   use {"akinsho/toggleterm.nvim", config = "require('toggleterm-config')"}
-  use {"terrortylor/nvim-comment", config = "require('comment-config')", cmd = "CommentToggle"}
-  use 'tami5/lspsaga.nvim' 
+  use {"terrortylor/nvim-comment", config = "require('comment-config')"}
+  use {'tami5/lspsaga.nvim', config = "require('lspsaga-config')"}
+  use {'williamboman/nvim-lsp-installer'}
+  use {'jose-elias-alvarez/null-ls.nvim', config = "require('null-ls-config')"}
+  use {"folke/zen-mode.nvim", config = 'require("zen-mode-config")'}
+  use {"folke/twilight.nvim", config = "require('twilight-config')"}
+  use {'andweeb/presence.nvim', config = "require('presence-config')"}
   use 'szw/vim-maximizer'
   use 'puremourning/vimspector'
   use 'Shougo/vimproc.vim'
