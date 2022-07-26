d6a4ab71c2451eb6f5db922ea3f14a07fcd7d3fc
JoTerrance <gorka.zgz@gmail.com>
2022-07-26 22:56:40 +0200

    refactor fixing bugs

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ curl https://bootstrap.pypa.io/get-pip.py -o $HOME/get-pip.py
 sudo python3 $HOME/get-pip.py 
 pip3 install ansible
 pip3 install ansible-base
-ansible-playbook ./Student/localhost-deploy.yml --ask-become-pass
+ansible-playbook ./Vagrant/localhost-deploy.yml --ask-become-pass
