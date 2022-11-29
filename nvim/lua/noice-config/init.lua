0b8b6da321ceb0c4aa5cd84826f5334176c21f25
Gorka Sanz <gorka.sanz@salesianos.edu>
2022-11-29 12:28:01 +0100

    fixed leap spooky bug

--- nvim/lua/noice-config/init.lua
+++ nvim/lua/noice-config/init.lua
@@ -31,7 +31,7 @@ local default = {
     enabled = true, -- enables the Noice messages UI
     view = "notify", -- default view for messages
     view_error = "notify", -- view for errors
-    view_warn = "notify", -- view for warnings
+    view_warn = "split", -- view for warnings
     view_history = "split", -- view for :messages
     view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
   },
