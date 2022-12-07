1d5a58948b7ecd060a6cd76d3c3e62cbb640a5b2
JoTerrance <gorka.zgz@gmail.com>
2022-12-07 17:08:04 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 sudo apt update
 # esto seguro que funciona
-sudo apt install curl
+sudo apt install curl -y
 curl https://bootstrap.pypa.io/get-pip.py -o $HOME/get-pip.py  
 sudo python3 $HOME/get-pip.py 
 pip3 install ansible
 pip3 install ansible-base
-sudo apt install ansible
+sudo apt install ansible -y
 if [ -z "${CODESPACE_NAME}" ] then 
   ansible-playbook ./Vagrant/localhost-deploy.yml --ask-become-pass
 else
