e02ae503ed4e1d2e1e5dcdf4a5c2a321fecfac69
JoTerrance <gorka.zgz@gmail.com>
2022-12-07 16:56:34 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,4 +6,9 @@ curl https://bootstrap.pypa.io/get-pip.py -o $HOME/get-pip.py
 sudo python3 $HOME/get-pip.py 
 pip3 install ansible
 pip3 install ansible-base
-ansible-playbook ./Vagrant/localhost-deploy.yml --ask-become-pass
+sudo apt install ansible
+if [ -z "${CODESPACE_NAME}" ] then 
+  ansible-playbook ./Vagrant/localhost-deploy.yml --ask-become-pass
+else
+  ansible-playbook ./Vagrant/minimal-deploy.yml --ask-become-pass
+fi
