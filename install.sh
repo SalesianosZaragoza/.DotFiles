bf17f92ec426aa82dcb039dbb975eb800536950a
JoTerrance <gorka.zgz@gmail.com>
2022-12-07 17:29:51 +0100

    minimal config (#7)
    * Update install.sh
    
    * Update install.sh
    
    * Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 sudo apt update
 # esto seguro que funciona
-sudo apt install curl
+sudo apt install curl -y
 curl https://bootstrap.pypa.io/get-pip.py -o $HOME/get-pip.py  
 sudo python3 $HOME/get-pip.py 
 pip3 install ansible
 pip3 install ansible-base
-sudo apt install ansible
-if [ -z "${CODESPACE_NAME}" ] then 
+sudo apt install ansible -y
+if [ -z "${CODESPACE_NAME}" ]; then 
   ansible-playbook ./Vagrant/localhost-deploy.yml --ask-become-pass
 else
-  ansible-playbook ./Vagrant/minimal-deploy.yml --ask-become-pass
+  ansible-playbook ./Vagrant/minimal-deploy.yml
 fi
