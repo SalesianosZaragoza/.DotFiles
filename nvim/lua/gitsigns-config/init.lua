db295074a073df8c3cee13933dd34eaa3f846f80
JoTerrance <gorka.zgz@gmail.com>
2022-12-20 21:12:26 +0100

    Update init.lua

--- nvim/lua/gitsigns-config/init.lua
+++ nvim/lua/gitsigns-config/init.lua
@@ -1,1 +1,10 @@
-require('gitsigns').setup {current_line_blame = not vim.visual_impairing }
+require('gitsigns').setup {
+  current_line_blame = not vim.visual_impairing,
+  signs = {
+    add = { text = '+' },
+    change = { text = '~' },
+    delete = { text = '_' },
+    topdelete = { text = '‾' },
+    changedelete = { text = '~' },
+  },
+}
