18edc14dc6ecec12c857f566ea0ddfa1f5c11bfb
JoTerrance <gorka.zgz@gmail.com>
2022-12-20 21:33:49 +0100

    Update init.lua

--- nvim/lua/options/init.lua
+++ nvim/lua/options/init.lua
@@ -57,3 +57,13 @@ vim.bo.expandtab = true
 vim.opt.termguicolors = true
 vim.cmd[[colorscheme tokyonight]]
 vim.g.tokyonight_style = 'night'
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
