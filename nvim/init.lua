b43eb1f245bf79d077a24805bc14fa566ebc7069
JoTerrance <gorka.zgz@gmail.com>
2022-12-20 20:07:57 +0100

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -26,3 +26,12 @@ end
 require('dial-config')
 require('leap-config')
 require('surround-config')
+
+-- Automatically source and re-compile packer whenever you save this init.lua
+local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
+vim.api.nvim_create_autocmd('BufWritePost', {
+  command = 'source <afile> | PackerCompile',
+  group = packer_group,
+  pattern = vim.fn.expand '$MYVIMRC',
+})
+
