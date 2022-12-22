7b6a36f3b02ee6a00f1d008ba71d31d460cf148c
JoTerrance <gorka.sanz@salesianos.edu>
2022-12-22 11:34:09 +0100

    fixing bugs

--- nvim/lua/lsp/cmp.lua
+++ nvim/lua/lsp/cmp.lua
@@ -1,10 +1,15 @@
 local lspkind = require "lspkind"
 local cmp = require "cmp"
 local luasnip = require 'luasnip'
+
 cmp.setup {
+  enabled = function()
+    return vim.api.nvim_buf_get_option(0, "buftype") ~= "prompt"
+        or require("cmp_dap").is_dap_buffer()
+  end,
   mapping = {
-    ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-d>"] = cmp.mapping.scroll_docs(4),
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-d>'] = cmp.mapping.scroll_docs(4),
     ["<C-c>"] = cmp.mapping.close(),
     ['<Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
@@ -24,7 +29,7 @@ cmp.setup {
         fallback()
       end
     end, { 'i', 's' }),
-    ["<CR>"] = cmp.mapping(
+    ['<CR>'] = cmp.mapping(
       cmp.mapping.confirm {
         behavior = cmp.ConfirmBehavior.Insert,
         select = true,
@@ -32,88 +37,65 @@ cmp.setup {
       { "i", "c" }
     ),
     ['<C-Space>'] = cmp.mapping.complete(),
-    ["<S-CR>"] = cmp.mapping.confirm {
+    ['<S-CR>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     },
-
-  sources = {
-    { name = "gh_issues" },
-
-    -- Youtube: Could enable this only for lua, but nvim_lua handles that already.
-    { name = "nvim_lua" },
-    { name = 'cmp_tabnine' },
-    { name = "nvim_lsp" },
-    { name = "copilot" },
-    { name = "treesitter" },
-    { name = "path" },
-    { name = "rg" },
-    { name = "vsnip" },
-    { name = "buffer", keyword_length = 5 },
-  },
-
-  sorting = {
-    -- TODO: Would be cool to add stuff like "See variable names before method names" in rust, or something like that.
-    comparators = {
-      cmp.config.compare.offset,
-      cmp.config.compare.exact,
-      cmp.config.compare.score,
-
-      -- copied from cmp-under, but I don't think I need the plugin for this.
-      -- I might add some more of my own.
-      function(entry1, entry2)
-        local _, entry1_under = entry1.completion_item.label:find "^_+"
-        local _, entry2_under = entry2.completion_item.label:find "^_+"
-        entry1_under = entry1_under or 0
-        entry2_under = entry2_under or 0
-        if entry1_under > entry2_under then
-          return false
-        elseif entry1_under < entry2_under then
-          return true
-        end
-      end,
-
-      cmp.config.compare.kind,
-      cmp.config.compare.sort_text,
-      cmp.config.compare.length,
-      cmp.config.compare.order,
+     snippet = {
+      expand = function(args)
+        require'luasnip'.lsp_expand(args.body)
+      end
     },
-  },
+     sources = cmp.config.sources({
+      { name = 'gh_issues' },
+      { name = 'nvim_lua' },
+      { name = 'cmp_tabnine' },
+      { name = 'nvim_lsp' },
+      { name = 'copilot' },
+      { name = 'treesitter' },
+      { name = 'luasnip' },
+      { name = 'path' },
+      { name = 'rg' },
+      { name = 'vsnip' },
+      { name = 'buffer', keyword_length = 5 },
+      { name = 'plugins' },
+      { name = 'tags' },
+      { name = 'npm', keyword_length = 4 },
+      { name = 'zsh' },
+      { name = 'nvim_lsp_signature_help' },
+      { name = 'nvim_lsp_document_symbol' },
+      {
+			name = "dictionary",
+			keyword_length = 2,
+		},
+       {
+            name = 'spell',
+            option = {
+                keep_all_entries = false,
+                enable_in_context = function()
+                    return true
+                end,
+            },
+        },
+       {
+            name = 'look',
+            keyword_length = 2,
+            option = {
+                convert_case = true,
+                loud = true
+                --dict = '/usr/share/dict/words'
+            }
+        },
 
-  -- Youtube: mention that you need a separate snippets plugin
-  snippet = {
-    expand = function(args)
-      require("luasnip").lsp_expand(args.body)
-    end,
+      -- { name = 'luasnip' }, -- For luasnip users.
+      -- { name = 'ultisnips' }, -- For ultisnips users.
+      -- { name = 'snippy' }, -- For snippy users.
+    }, {
+      { name = 'buffer' },
+    })
+ --   sources = {
+ --   },
   },
-
-  formatting = {
-    -- Youtube: How to set up nice formatting for your sources.
-    format = lspkind.cmp_format {
-      mode = 'symbol_text',
-      preset = 'default',
-      menu = {
-        buffer = "[buf]",
-        treesitter = "[ts]",
-        nvim_lsp = "[LSP]",
-        nvim_lua = "[api]",
-        path = "[path]",
-        vsnip = "[snip]",
-        gh_issues = "[issues]",
-        cmp_tabnine = "[TN]",
-        copilot = "[copilot]",
-      },
-    },
-  },
-
-  experimental = {
-    -- I like the new menu better! Nice work hrsh7th
-    native_menu = false,
-
-    -- Let's play with this for a day or two
-    ghost_text = false,
-  },
-},
 }
 
 cmp.setup.cmdline("/", {
@@ -141,6 +123,18 @@ cmp.setup.cmdline(":", {
       keyword_length = 4,
     },
   }),
+})
+
+require("cmp").setup.filetype({ "dap-repl", "dapui_watches", "dapui_hover" }, {
+  sources = {
+    { name = "dap" },
+  },
+})
+
+cmp.setup.filetype('gitcommit', {
+  sources = {
+    { name = 'commit' }
+  }
 })
 
 --[[
@@ -176,4 +170,3 @@ _ = vim.cmd [[
 " Disable cmp for a buffer
 autocmd FileType TelescopePrompt lua require('cmp').setup.buffer { enabled = false }
 --]]
-
