95e20119c67de63497945c3bdc3d26c6fc1d92ae
JoTerrance <gorka.zgz@gmail.com>
2022-12-22 11:24:48 +0100

    Update init.lua

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -140,7 +140,6 @@ return require('packer').startup(function(use)
     disable = vscode
   })
   use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/nvim-cmp'
   use 'hrsh7th/cmp-vsnip'
   use 'hrsh7th/vim-vsnip'
   use 'lukas-reineke/cmp-rg'
