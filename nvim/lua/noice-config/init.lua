86dfab6ff58ba25017a26d7acf6f3ee520b3933c
JoTerrance <gorka.sanz@salesianos.edu>
2023-01-20 12:05:45 +0100

    updated noice to use view mini

--- nvim/lua/noice-config/init.lua
+++ nvim/lua/noice-config/init.lua
@@ -29,9 +29,9 @@ local default = {
     -- NOTE: If you enable messages, then the cmdline is enabled automatically.
     -- This is a current Neovim limitation.
     enabled = true, -- enables the Noice messages UI
-    view = "split", -- default view for messages
-    view_error = "split", -- view for errors
-    view_warn = "split", -- view for warnings
+    view = "mini", -- default view for messages
+    view_error = "mini", -- view for errors
+    view_warn = "mini", -- view for warnings
     view_history = "split", -- view for :messages
     view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
   },
