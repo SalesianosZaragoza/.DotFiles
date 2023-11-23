3f62a21eb0561bdeb9f9ab3233c618f526e2e18a
JoTerrance <gorka.sanz@salesianos.edu>
2023-11-23 11:55:41 +0100

    cleanup

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo python3 $HOME/get-pip.py
 pip3 install ansible
 pip3 install ansible-base
 sudo apt install ansible -y
-if [ -z "${CODESPACE_NAME}" ]; then 
+if [ -z "$CODESPACE_NAME" ]; then 
   ansible-playbook ./Vagrant/localhost-deploy.yml --ask-become-pass
 else
   ansible-playbook ./Vagrant/minimal-deploy.yml
